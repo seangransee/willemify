020c46eab7158613463006cf4e6e4b1a3024879c
Joseph Klonowski <klonowskijoseph@gmail.com>
2014-11-22 22:57:42 -0600

    Images now have ids
    img0, img1, etc

--- WEB/js/web.js
+++ WEB/js/web.js
@@ -1,9 +1,11 @@
-
+var counter = 0;
 $(function() {
 	Dropzone.instances[0].on("addedfile", function(file) {
 
     	var img = document.createElement('img');
         img.file = file;
+        img.id = "img" + counter;
+        counter ++;
         document.body.appendChild(img);
         var reader = new FileReader();
         reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
